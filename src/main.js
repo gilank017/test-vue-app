eaf25dc3f2168c9c281ae550bb4b3c35128a0a4b
gilank017 <gilank.zone@gmail.com>
2022-07-09 02:35:42 +0700

    delete vuex

--- src/main.js
+++ src/main.js
@@ -3,12 +3,10 @@ import Home from './page/home-page.vue'
 import jQuery from 'jquery'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Store from './store'
 
 global.$ = jQuery
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(Home),
-  Store,
 }).$mount('#app')
